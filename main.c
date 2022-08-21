47b9c19d4d485b0bd5089686b148b06687ab90a0
aashkadel99 <kadelaashish22@gmail.com>
2022-08-21 19:17:01 +0545

    Bug fixes related to draw case

--- main.c
+++ main.c
@@ -11,24 +11,26 @@ int main(){
 	
 	int tries = 3, x, y;
 	
-	x = userchoice();
-	y = computerChoice()
+	x = userChoice();
+	y = computerChoice();
 	
 	if (evaluateWinner(x,y) == 'P'){
-		printf("Player wins this round.")
+		printf("Player wins this round.\n");
+	} else if (evaluateWinner(x,y) == 'C') {
+		printf("Computer wins this round.\n");
 	} else {
-		printf("Computer wins this round.")
+		printf("It is a Draw.");
 	}
 	
 	return 0;
 }
 
 void displayChoices(){
-	printf("*************");
+	printf("*************\n");
 	printf("1. Rock\n");
 	printf("2. Paper\n");
 	printf("3. Scissors\n");
-	printf("*************");
+	printf("*************\n");
 }
 
 int userChoice(){
@@ -36,21 +38,43 @@ int userChoice(){
 	displayChoices();
 	printf("Enter your play: ");
 	scanf("%d", &play);
+	
+	if(play == 1){
+		printf("Player chose Rock.\n");
+	} else if (play == 2){
+		printf("Player chose Paper.\n");
+	} else {
+		printf("Player chose Scissors.\n");
+	}
+	
 	return play;
 }
 
 int computerChoice(){
 	int number;
+	
 	srand(time(0));
 	number = (rand() % 3) + 1;
+	
+	if(number == 1){
+		printf("Computer chooses Rock.\n");
+	} else if (number == 2){
+		printf("Computer chooses Paper.\n");
+	} else {
+		printf("Computer chooses Scissors.\n");
+	}
+	
 	return number;
 }
 
 char evaluateWinner(int x, int y){
-	if ((x == 1 && y == 3) || (x == 2) && (y == 1) || (x == 3) && (y == 2)){
+	
+	if ((x == 1 && y == 3) || (x == 2 && y == 1) || (x == 3 && y == 2)){
 		return 'P';
-	} else {
+	} else if ((x == 3 && y == 1) || (x == 1 && y == 2) || (x == 2 && y == 3)){
 		return 'C';
+	} else {
+		return 'D';
 	}
 }
 
