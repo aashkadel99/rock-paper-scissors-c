d79fe1e66c4e6073b44228dd9b4fccb4e2786ae6
aashkadel99 <kadelaashish22@gmail.com>
2022-08-21 19:00:45 +0545

    Added functions

--- main.c
+++ main.c
@@ -1,7 +1,49 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void displayChoices();
+int userChoice();
+int computerChoice();
+
+
 int main(){
+	
+	int tries = 3, x, y;
+	
+	x = userchoice();
+	y = computerChoice()
+	
+	if (x == 1 and y == 2){
+			
+	}
+	
 	
 	
 	
 	return 0;
 }
+
+void displayChoices(){
+	printf("*************");
+	printf("1. Rock\n");
+	printf("2. Paper\n");
+	printf("3. Scissors\n");
+	printf("*************");
+}
+
+int userChoice(){
+	int play;
+	displayChoices();
+	printf("Enter your play: ");
+	scanf("%d", &play);
+	return play;
+}
+
+int computerChoice(){
+	int number;
+	srand(time(0));
+	number = (rand() % 3) + 1;
+	return number;
+}
+
+
